08d0b8cd8a25e5ee46ac2116a48c789f148a068b
Ryan Leckey <rleckey@concordusapps.com>
2012-10-24 02:48:14 -0700

    Initial work on fixing http.httpResposne

--- src/flapjack/http.py
+++ src/flapjack/http.py
@@ -7,11 +7,9 @@ class HttpResponse(http.HttpResponse):
     """ ..
     """
 
-    def serialize(self):
+    def __init__(self, *args, **kwargs):
+        super(HttpResponse, self).__init__(*args, **kwargs)
         if not self.content:
             # No need to specify the default content-type if we don't
             # have a body.
             del self['Content-Type']
-
-        # Delegate to django to serialize ourself
-        super(HttpResponse, self).serialize()
