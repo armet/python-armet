bc9b99ea8adf2bf777629133f290eadd04db93b4
Ryan Leckey <rleckey@concordusapps.com>
2012-10-29 05:04:37 -0700

    Made installable by pip.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+        name='flapjack',
+        version=read('version'),
+        description=read('description'),
+        long_description=read('README.md'),
+        author='Concordus Applications',
+        author_email='support@concordusapps.com',
+        url='http://github.com/concordusapps/django-flapjack',
+        package_dir={'flapjack': 'src/flapjack'},
+        packages=find_packages('src')
+    )
