4f6137b9aebf8c035f6cbc7fe2992e2a993ff938
Ryan Leckey <rleckey@concordusapps.com>
2012-11-10 14:05:22 -0800

    Naive implementation of `read`

--- src/flapjack/resources/model.py
+++ src/flapjack/resources/model.py
@@ -8,8 +8,32 @@ from .base import Resource, Meta
 
 
 class Meta(Meta):
-    pass
+
+    def __new__(cls, name, bases, attrs):
+        """
+        """
+        # construct the class object.
+        obj = super(Meta, cls).__new__(cls, name, bases, attrs)
+
+        # return the constructed object; wipe off the magic -- not really.
+        return obj
 
 
 class Resource(six.with_metaclass(Meta, Resource)):
-    pass
+    """
+    Implements a resource using bindings from django's ORM layer to
+    simplify and pre-define every operation with sane defaults.
+    """
+
+    #! The django model to bind to for the ORM layer to work.
+    model = None
+
+    @property
+    def queryset(self):
+        return self.model.objects.all()
+
+    def read(self):
+        if self.identifier is not None:
+            return self.queryset.filter(pk=self.identifier)
+
+        return self.queryset.all()
