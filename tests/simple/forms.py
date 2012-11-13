70cfae020a902e040858a491d41d382259724ad4
Ryan Leckey <rleckey@concordusapps.com>
2012-11-12 23:16:15 -0800

    Added some forms.

--- tests/simple/forms.py
+++ tests/simple/forms.py
@@ -2,7 +2,15 @@
 """ Defines forms for this sample project.
 """
 from django import forms
+from . import models
 
 
 class Poll(forms.Form):
-    pass
+    question = forms.CharField()
+    booths = forms.MultipleChoiceField((1, 2, 3, 4, 5))
+
+
+# class Poll(forms.ModelForm):
+#     class Meta:
+#         model = models.Poll
+#     blue = forms.CharField()
