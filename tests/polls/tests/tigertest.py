90bee173fc3e25ef396358cafdf9c34ab2691f95
avoliva <avoliva@concordusapps.com>
2012-11-19 14:45:15 -0800

    Updated requirements.

--- tests/polls/tests/tigertest.py
+++ tests/polls/tests/tigertest.py
@@ -1,13 +1,14 @@
+from tastypie.test import ResourceTestCase
 from django.utils import unittest
 from django.test.client import Client
 
-class TigerTest(unittest.TestCase):
+class TigerTest(ResourceTestCase):
     """Unit Tests for the Are You A Tiger poll object"""
-    
+
     def setUp(self):
         """Set up the django HTTP client"""
         self.c = Client()
-        
+
     def test_list_view_xml(self):
         """Gets the list view in xml format"""
         self.response = self.c.get('/api/v1/poll.xml/')
@@ -17,6 +18,8 @@ class TigerTest(unittest.TestCase):
         """Gets the list view in json format"""
         self.response = self.c.get('/api/v1/poll.json/')
         self.assertEqual(self.response.status_code, 200)
+        self.assertValidJSONResponse(self.response)
+        self.assertValidJSON(self.response.content)
 
     def test_list_view_text(self):
         """Gets the list view in text format"""
