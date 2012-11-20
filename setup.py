f60f21ec3e49ddb8c1600e1154f5e41bdd68a5e7
Ryan Leckey <rleckey@concordusapps.com>
2012-11-19 22:37:20 -0800

    Cleaned up setup script.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 
 def read(filename):
-    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read().strip()
 
 
 setup(
         name='flapjack',
         version=read('VERSION'),
-        description='Clean and modern framework in django for creating '
-                    'RESTful APIs.',
+        description=read('description'),
         long_description=read('README.md'),
         author='Concordus Applications',
         author_email='support@concordusapps.com',
