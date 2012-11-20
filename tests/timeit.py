e35c81a12130c847ae7e748e36faff0f428d19ba
Ryan Leckey <rleckey@concordusapps.com>
2012-11-20 06:35:48 -0800

    Added speed test.

---
+++ tests/timeit.py
@@ -0,0 +1,19 @@
+import requests
+import datetime
+import time
+import numpy
+
+
+REQUESTS = 100
+
+
+times = []
+for x in range(1, REQUESTS):
+    start = time.time()
+    x = requests.get('http://localhost:8000/api/choice')
+    times.append(time.time() - start)
+
+print 'requests: {}'.format(REQUESTS)
+print 'total: {}'.format(sum(times))
+print 'average: {}'.format(numpy.mean(times))
+print '\n'
