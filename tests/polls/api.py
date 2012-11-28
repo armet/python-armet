6356b12b320367ba6b951f76eec6e82e7675aef5
James Miles <jmiles@concordusapps.com>
2012-11-28 15:32:31 -0800

    polls is using an API(), rather than urls directly from a resource

--- tests/polls/api.py
+++ tests/polls/api.py
@@ -2,26 +2,44 @@
 """ Defines the RESTful interface for this sample project.
 """
 from flapjack import resources
+from flapjack.resources import field, relation
 from . import models
+from flapjack.api import Api
 
-class Choice(resources.Model):
-    #class Meta:
+
+class ChoiceWithPollExcluded(resources.Model):
     model = models.Choice
-#    relations = {'poll': 'polls.api.Poll'}
-#    name = 'answer'
+    resource_uri = None
+    exclude = 'poll',
+
 
 class Poll(resources.Model):
-    model = models.Poll
-    relations = {'choice_set': 'polls.api.Choice'}
-#    include = 'answers'
 
-#    def prepare_answers(self,obj,value):
-#         return [str(x) for x in obj.choice_set.all()]
-#         return obj.choices.all()
+    model = models.Poll
+    include = {
+        'answers': field('choice_set'),
+        'total_votes': field('choice_set')
+    }
+
+    relations = {
+        'answers': relation(ChoiceWithPollExcluded, embed=True, local=True)
+    }
+
+
+    #TODO: Somebody please replace this with a faster sum function
+    def prepare_total_votes(self,obj,val):
+        votes = 0
+        for i in val:
+            votes += i.votes
+        return votes
+
+apiv6 = Api("v6")
+
+apiv6.register(Poll)
 
 
 #Issue #1:  inverse relationships cannot be accessed via API.
 
-#Issue #2:  relations requires that the resource being linked to must have an exposed URL.
+#Issue #2:  relations requires that the resource being linked to must have an exposed URL.  --fixed!
 
 #Enhancement:  We should automatically create resources for models.
--- tests/polls/urls.py
+++ tests/polls/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # Resources
-    url(r'^api/', include(api.Poll.urls)),
+#    url(r'^api/', include(api.Poll.urls)),
+    url(r'^api/', include(api.apiv6.urls)),
     # url(r'^api/', include(api.Booth.urls))
 )
