f4aa695df02220b4eb9788c4a87f8629b1d06883
Erich Healy <ErichRHealy@gmail.com>
2012-12-04 22:35:41 -0800

    Cleaned up the API registry

--- src/flapjack/api.py
+++ src/flapjack/api.py
@@ -1,26 +1,74 @@
+# -*- coding: utf-8 -*-
+"""Defines the api registry used to register and manage resources
+"""
 from django.conf.urls import patterns, url, include
 from flapjack import resources
 
+
 class Api(resources.Resource):
+    """An api registry used to wrangle apis
+    """
+
+    allowed_operations = ('read')
+
     def __init__(self, api_name="v0"):
         # set the api name
-        self.api_name = api_name
-        self._registry = {}
+        self.name = api_name
+        self._registry = []
 
     def register(self, resource):
-        # take a class object as a parameter.
-        # add the resource to a vector.
-        self._registry[resource.name] = resource
+        """take a class object as a parameter.
+        add the resource to a registry.
+        """
+        self._registry.append(resource)
 
-    def unregister(self,resource_name):
-        #delete from vector
-        del self._registry[resource_name]
+    def unregister(self, resource):
+        """delete from registry
+        """
+        self._registry.remove(resource)
 
     @property
     def urls(self):
-        pattern = []
-        for resource in self._registry.values():
-            pattern += patterns('', url(r'^{}/'.format(self.api_name), include(resource.urls)))
+        """Returns a url list of all resources registered with this resource
+        """
+        return patterns('', [self.urlify(x) for x in self._registry])
 
-        return pattern
-         # [str(x) for x in ]
+    def urlify(self, resource):
+        """Takes a resource list returns a corresponding url object
+        """
+        return url(r'{}/'.format(self.name), include(resource.urls))
+
+    def __setitem__(self, key, value):
+        """Implement a Sequence method
+        """
+        self._registry[key] = value
+
+    def __delitem__(self, key):
+        """Implement a sequence method
+        """
+        del self._registry[key]
+
+    def insert(self, key, item):
+        """Implement a sequence method
+        """
+        self._registry.insert(key, item)
+
+    def __getitem__(self, index):
+        """Implement a sequence method
+        """
+        return self._registry[index]
+
+    def __len__(self):
+        """Implement a sequence method
+        """
+        return len(self._registry)
+
+    def __iter__(self):
+        """Implement a sequence method
+        """
+        return self._registry
+
+    def __contains__(self, item):
+        """Implement a conditional method
+        """
+        return item in self._registry
