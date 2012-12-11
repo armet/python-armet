5ed3372b096fc9e1172d1512c32924c44d2f3bf1
Ryan Leckey <rleckey@concordusapps.com>
2012-12-11 00:35:13 -0800

    Small bug fixed and comments added to proxies.

--- src/armet/resources/proxy.py
+++ src/armet/resources/proxy.py
@@ -14,10 +14,12 @@ class NumericProxy(object):
 
     @property
     def numerator(obj):
+        """The numerator of this number."""
         return obj.numerator
 
     @property
     def denominator(obj):
+        """The denominator of this number."""
         return obj.denominator
 
 
@@ -25,10 +27,12 @@ class ComplexProxy(object):
 
     @property
     def real(obj):
+        """The real part of this complex number."""
         return obj.real
 
     @property
     def imag(obj):
+        """The imaginary part of this complex number."""
         return obj.imag
 
 
@@ -45,6 +49,7 @@ class StringProxy(collections.Sequence):
 
     @property
     def length(obj):
+        """The number of characters in this string."""
         return len(obj)
 
 
@@ -52,6 +57,7 @@ class TemporalProxy(object):
 
     @property
     def ordinal(obj):
+        """The proleptic Gregorian ordinal of this date/time."""
         return obj.toordinal()
 
 
@@ -59,22 +65,27 @@ class DateProxy(TemporalProxy):
 
     @property
     def year(obj):
-        return obj.month
+        """The 4-digit year."""
+        return obj.year
 
     @property
     def month(obj):
+        """The month from 1 to 12."""
         return obj.month
 
     @property
     def day(obj):
+        """The day of the month from 1 to the number of days in the month."""
         return obj.day
 
     @property
     def weekday(obj):
+        """The day of the week where Monday is 1 and Sunday is 7 (ISO)."""
         return obj.isoweekday()
 
     @property
     def yearday(obj):
+        """The day of the year from 1 to 366."""
         return obj.timetuple().tm_yday
 
 
@@ -82,36 +93,45 @@ class TimeProxy(TemporalProxy):
 
     @property
     def hour(obj):
+        """The hour of this date/time."""
         return obj.hour
 
     @property
     def minute(obj):
+        """The minute of this date/time."""
         return obj.minute
 
     @property
     def second(obj):
+        """The second of this date/time."""
         return obj.second
 
     @property
     def millisecond(obj):
+        """The fractional part of the second of this date/time, in ms."""
         return obj.microsecond / 1000.0
 
     @property
     def microsecond(obj):
+        """The fractional part of the second of this date/time, in us."""
         return obj.microsecond
 
     @property
     def dst(obj):
+        """The applied offset for daylight savings time, if in effect."""
         value = obj.dst()
         return value if value is not None else datetime.timedelta()
 
     @property
     def timezone(obj):
+        """The name of the timezone in which this date/time is defined."""
         return obj.tzname()
 
     @property
     def offset(obj):
-        return obj.utcoffset()
+        """The offset from UTC for the timezone this date/time is defined."""
+        value = obj.utcoffset()
+        return value if value is not None else datetime.timedelta()
 
 
 class DateTimeProxy(DateProxy, TimeProxy):
