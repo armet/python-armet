f25f9bd3d33e04d7bcb6b8f4f655e18214bd95ad
Hardy Jones <hjones@concordusapps.com>
2013-01-04 15:56:44 -0800

    Test case for Content-MD5 response.

---
+++ src/armet/tests/test_response.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from hashlib import md5
+from armet.utils import test
+
+
+class ResponseTestCase(test.TestCase):
+
+    def setUp(self):
+        super(ResponseTestCase, self).setUp()
+        self.endpoint = '/'
+
+    def test_content_md5(self):
+        # Check some random endpoint
+        endpoint = '{}choice/1'.format(self.endpoint)
+        response = self.client.get(endpoint)
+        # Assert we got a Content-MD5 header
+        self.assertTrue(response.has_header('Content-MD5'))
+        # Make an MD5 of the body.
+        md5_body = md5(response.content).hexdigest()
+        # Assert the MD5 is correct.
+        self.assertEqual(response['Content-MD5'], md5_body)
