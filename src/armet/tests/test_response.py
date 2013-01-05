6d91034972a2b085ac2458c87247597a9d43a482
Hardy Jones <hjones@concordusapps.com>
2013-01-04 16:27:21 -0800

    Added case for empty body, but fails due to 404 template.

--- src/armet/tests/test_response.py
+++ src/armet/tests/test_response.py
@@ -19,3 +19,14 @@ class ResponseTestCase(test.TestCase):
         md5_body = hashlib.md5(response.content).hexdigest()
         # Assert the MD5 is correct.
         self.assertEqual(response['Content-MD5'], md5_body)
+        # This fails with 404 for now (no template)
+        # # Check a blank content
+        # # should be 'd41d8cd98f00b204e9800998ecf8427e'
+        # endpoint = '{}choice/thisendpointisbunk'
+        # response = self.client.get(endpoint)
+        # # Assert we got a Content-MD5 header
+        # self.assertTrue(response.has_header('Content-MD5'))
+        # # Make an MD5 of the body.
+        # md5_body = hashlib.md5(response.content).hexdigest()
+        # # Assert the MD5 is correct.
+        # self.assertEqual(response['Content-MD5'], md5_body)
