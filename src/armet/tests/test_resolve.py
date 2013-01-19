7e0c7011f7f3a0d98fc0eca0c51140bda03bf398
Hardy Jones <hjones@concordusapps.com>
2013-01-18 19:42:24 -0800

    Added more tests for polls.

--- src/armet/tests/test_resolve.py
+++ src/armet/tests/test_resolve.py
@@ -17,10 +17,12 @@ class ResolveTestCase(test.TestCase):
 
     def test_resolve_poll(self):
         """Test the resolvability of a poll url."""
+
         # Test all the polls.
         request = RequestFactory().get(self.poll_endpoint)
         objs = Resolve(request=request).resolve(request.path)
         self.assertItemsEqual(Poll.objects.all(), objs)
+
         # Test a specific poll.
         poll_number = 1
         request = RequestFactory().get('{}/{}'.format(self.poll_endpoint,
@@ -28,6 +30,32 @@ class ResolveTestCase(test.TestCase):
         # Returns a list, so grab the first one.
         obj = Resolve(request=request).resolve(request.path)[0]
         self.assertEquals(Poll.objects.get(pk=poll_number), obj)
+
+        # Try that again as a query string with id.
+        poll_number = 1
+        request = RequestFactory().get('{}?id={}'.format(self.poll_endpoint,
+                                                         poll_number))
+        # Returns a list, so grab the first one.
+        obj = Resolve(request=request).resolve(request.path)[0]
+        self.assertEquals(Poll.objects.get(pk=poll_number), obj)
+
+        # Try that again as a query string with pk.
+        poll_number = 1
+        request = RequestFactory().get('{}?pk={}'.format(self.poll_endpoint,
+                                                         poll_number))
+        # Returns a list, so grab the first one.
+        obj = Resolve(request=request).resolve(request.path)[0]
+        self.assertEquals(Poll.objects.get(pk=poll_number), obj)
+
+        # Should check the question here, but it's not working.
+
+        # Try a pub_date.
+        poll_date = '2012-12-11T16:20:46.016Z'
+        query_string = '{}?pub_date={}'.format(self.poll_endpoint, poll_date)
+        request = RequestFactory().get(query_string)
+        # Returns a list, so grab the first one.
+        obj = Resolve(request=request).resolve(request.path)[0]
+        self.assertEquals(Poll.objects.get(pub_date=poll_date), obj)
 
     def test_resolve_choice(self):
         """Test the resolvability of a choice url."""
@@ -39,6 +67,22 @@ class ResolveTestCase(test.TestCase):
         choice_number = 1
         request = RequestFactory().get('{}/{}'.format(self.choice_endpoint,
                                                       choice_number))
+        # Returns a list, so grab the first one.
+        obj = Resolve(request=request).resolve(request.path)[0]
+        self.assertEquals(Choice.objects.get(pk=choice_number), obj)
+
+        # Try that again as a query string with id.
+        choice_number = 1
+        request = RequestFactory().get('{}?id={}'.format(self.choice_endpoint,
+                                                         choice_number))
+        # Returns a list, so grab the first one.
+        obj = Resolve(request=request).resolve(request.path)[0]
+        self.assertEquals(Choice.objects.get(pk=choice_number), obj)
+
+        # Try that again as a query string with pk.
+        choice_number = 1
+        request = RequestFactory().get('{}?pk={}'.format(self.choice_endpoint,
+                                                         choice_number))
         # Returns a list, so grab the first one.
         obj = Resolve(request=request).resolve(request.path)[0]
         self.assertEquals(Choice.objects.get(pk=choice_number), obj)
