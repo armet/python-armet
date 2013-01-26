62b7286a39bf0b00d756a8b3d59b6c5ba8471c74
avoliva <avoliva@concordusapps.com>
2013-01-25 19:33:32 -0800

    YAML encoder..........

--- src/armet/encoders/yaml.py
+++ src/armet/encoders/yaml.py
@@ -9,22 +9,50 @@ import yaml
 import six
 from .. import transcoders
 from . import Encoder, utils
+import types
+import collections
+import decimal
+import fractions
 
 
 class Encoder(transcoders.Yaml, Encoder):
 
-    def encode(self, obj=None):
-        if isinstance(obj, datetime.time) or isinstance(obj, datetime.datetime):
+    def _encode_value(self, obj):
+        if obj is None:
+            # We have nothing; and nothing in text is nothing.
+            return None
+
+        if isinstance(obj, datetime.time) or isinstance(obj, datetime.date):
             # This is some kind of date/time -- encode using ISO format.
-            obj = obj.isoformat()
+            return self._encode_value(obj.isoformat())
 
-        # Encode and return the resultant text
-        text = ''
+        if isinstance(obj, six.string_types):
+            # We have a string; simply return what we have.
+            return bytes(obj)
 
+        if isinstance(obj, collections.Mapping):
+            # Some kind of dictionary.
+            return {bytes(key): self._encode_value(obj[key]) for key in obj}
 
-        # text = yaml.dump(bytes(list(obj)), default_flow_style=True)
+        if isinstance(obj, collections.Sequence):
+            # Some kind of sequence.
+            return [self._encode_value(item) for item in obj]
 
-        print(obj)
+        if isinstance(obj, six.integer_types):
+            # Some kind of number.
+            return b'{}'.format(str(obj))
 
-        # Return our encoded result
-        return yaml.dump(bytes(list(obj)), default_flow_style=True)
+        if isinstance(obj, (float, fractions.Fraction, decimal.Decimal)):
+            # Some kind of something.
+            return b'{}'.format(str(obj))
+
+        if isinstance(obj, bool):
+            # Boolean
+            return b"true" if obj else b"false"
+
+        # We have no idea what we are..
+        return self._encode_value(utils.coerce_value(obj))
+
+    def encode(self, obj=None):
+        # Return the encoded text.
+        return yaml.dump(self._encode_value(obj), default_flow_style=True)
