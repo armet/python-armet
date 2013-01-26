0dc04a5c9007df16aacf91b1c4c21094862995f8
Ryan Leckey <rleckey@concordusapps.com>
2013-01-26 15:52:37 -0800

    Clarified some utilities and test stuff.

--- src/armet/utils/test.py
+++ src/armet/utils/test.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+Implementation of a test case with convenience assertions and facilities
+easing the unit testing of RESTful APIs.
 """
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
@@ -7,42 +9,21 @@ import collections
 from django.utils import unittest
 from django.core import management
 from django.test import client
-# from lxml import etree
-# import json
 from .. import http
-
-
-class Client(client.Client):
-
-    def get(self, path, *args, **kwargs):
-        return super(Client, self).get(path, *args, **kwargs)
-
-    def post(self, path, *args, **kwargs):
-        return super(Client, self).post(path, *args, **kwargs)
-
-    def put(self, path, *args, **kwargs):
-        return super(Client, self).put(path, *args, **kwargs)
-
-    def delete(self, path, *args, **kwargs):
-        return super(Client, self).delete(path, *args, **kwargs)
 
 
 class TestCase(unittest.TestCase):
 
     def setUp(self):
-        self.client = Client()
+        #! A web service client for making requests.
+        self.client = client.Client()
 
-    def assertHttpStatus(self, response, code_or_codes):
-        if isinstance(code_or_codes, collections.Iterable):
-            # Iterable of status codes; check if we're in
-            self.assertIn(response.status_code, code_or_codes)
-
-        else:
-            # Singular HTTP status code; check for it
-            self.assertEqual(response.status_code, code_or_codes)
+    def assertHttpStatus(self, response, *codes):
+        # Iterable of status codes; check if we're in.
+        self.assertIn(response.status_code, codes)
 
     def assertResponse(self, response, format='json', status=http.client.OK):
-        # Assert the status codes first
+        # Assert the status codes first.
         self.assertHttpStatus(response, status)
 
-        # TODO: Attempt to decode
+        # TODO: Attempt to decode...
