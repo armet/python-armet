02ab3141e27d9c6e3b5068f91d6f43d8a207fdf0
avoliva <adamvoliva@gmail.com>
2013-03-01 23:38:24 -0800

    XML Encoders works with lists now. Just needs to be able to encode a generator type.

--- src/armet/encoders/xml.py
+++ src/armet/encoders/xml.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
+from collections import Iterable, Mapping
 import xml.etree.ElementTree as ElementTree
+import datetime
 from .. import transcoders
 from . import Encoder
 
@@ -14,15 +16,24 @@ class Encoder(transcoders.Xml, Encoder):
             for key, value in obj.items():
                 if isinstance(value, int):
                     ElementTree.SubElement(root, key,
-                        type='integer').text = str(value)
+                        type='integer').text = bytes(str(value))
                 elif isinstance(value, dict):
                     text = self.dict_to_string(ElementTree.Element(key), value)
                     ElementTree.SubElement(root, key).text = text
+                elif isinstance(value, datetime.time) or isinstance(value, datetime.date):
+                    ElementTree.SubElement(root, key).text = value.isoformat()
+                elif isinstance(value, list):
+                    node = ElementTree.SubElement(root, key)
+                    print(node)
+                    for item in value:
+                        ElementTree.SubElement(node, key).text = item
                 else:
-                    ElementTree.SubElement(root, key).text = str(value)
+                    ElementTree.SubElement(root, key).text = bytes(str(value))
             xml = ElementTree.tostring(root)
             xml = xml.replace('&lt;', '<')
             xml = xml.replace('&gt;', '>')
+            if key == 'choices':
+                print(type(value))
         elif isinstance(obj, unicode):
             xml = ElementTree.Element('xml').text = obj
         else:
