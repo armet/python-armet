a4752f5355c19dec98895172d62015e67b836de1
Ryan Leckey <rleckey@concordusapps.com>
2013-03-19 08:48:50 -0700

    Removed requirements.txt files.

--- setup.py
+++ setup.py
@@ -1,35 +1,24 @@
 #! /usr/bin/env python
-import os
-import re
 from setuptools import setup, find_packages
-
-
-def read(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
-        return file.read().strip()
-
-
-def read_requirements(name):
-    filename = 'doc/requirements/{}.txt'.format(name)
-    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
-        result = []
-        for line in file.readlines():
-            result.append(re.match(r'^(.*?)#|$', line).groups()[0].strip())
-        return result
 
 
 setup(
     name='armet',
     version='0.3.0-pre',
     description='Clean and modern framework for creating RESTful APIs.',
-    long_description=read('README.md'),
     author='Concordus Applications',
     author_email='support@concordusapps.com',
     url='http://github.com/armet/python-armet',
     package_dir={'armet': 'src/armet'},
     packages=find_packages('src'),
-    install_requires=read_requirements('required'),
+    install_requires=(
+        'six', # Python 2 and 3 normalization layer
+        'python-mimeparse' # For parsing accept and content-type headers
+    ),
     extras_require={
-        'test': read_requirements('test'),
+        'test': (
+            'nose',
+            'yanc'
+        )
     }
 )
