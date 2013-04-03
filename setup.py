82023b73f936e0e0aefd712b39de7664c29653a1
Ryan Leckey <rleckey@concordusapps.com>
2013-04-03 06:40:33 -0700

    Restructure setup.py so it can only install what works in the running python version.

--- setup.py
+++ setup.py
@@ -1,5 +1,37 @@
 #! /usr/bin/env python
 from setuptools import setup, find_packages
+import sys
+
+
+#! True if we are running on Python 3.
+PY3 = sys.version_info[0] == 3
+
+
+# Required test dependencies.
+test_dependencies = (
+    # Test runner.
+    'nose',
+
+    # Colorized output for the test runner.
+    'yanc',
+
+    # Installs a WSGI application in place of a real URI for testing.
+    'wsgi_intercept == 0.6.0',
+
+    # HTTP request abstraction layer over httplib.
+    'httplib2',
+
+    # The Web framework for perfectionists with deadlines.
+    'django',
+)
+
+
+if not PY3:
+    # Test dependencies for python 2.x only.
+    test_dependencies += (
+        # A microframework based on Werkzeug, Jinja2 and good intentions.
+        'flask',
+    )
 
 
 setup(
@@ -11,18 +43,17 @@ setup(
     url='http://github.com/armet/python-armet',
     package_dir={'armet': 'src/armet'},
     packages=find_packages('src'),
+    dependency_links=(
+        'git+git://github.com/concordusapps/wsgi-intercept.git#egg=wsgi_intercept-0.6.0',
+    ),
     install_requires=(
-        'six',  # Python 2 and 3 normalization layer
-        'python-mimeparse'  # For parsing accept and content-type headers
+        # Python 2 and 3 normalization layer
+        'six',
+
+        # For parsing accept and content-type headers
+        'python-mimeparse'
     ),
     extras_require={
-        'test': (
-            'nose',
-            'yanc',
-            'httplib2',
-            'flask',
-            'django',
-            'wsgi_intercept'
-        )
+        'test': test_dependencies
     }
 )
