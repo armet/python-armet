61e16b47ba704150fa205a6e56ce37eef54ed9a5
Erich Healy <ErichRHealy@gmail.com>
2013-04-12 11:11:44 -0700

    Implement the cyclone request/response object

--- src/armet/connectors/cyclone/http.py
+++ src/armet/connectors/cyclone/http.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals, division
-from armet import utils
 from armet.http import request, response
 
 
@@ -8,21 +7,58 @@ class Request(request.Request):
     """Implements the request abstraction for cyclone.
     """
 
-    @property
-    @utils.memoize_single
-    def method(self):
-        pass
+    def __init__(self, handler):
+        self.handler = handler
 
-    def __getitem__(self):
-        pass
+        # This is the python request object
+        self.request = handler.request
+
+    @property
+    def url(self):
+        return self.request.full_url()
+
+    @property
+    def path(self):
+        return self.request.path
+
+    @path.setter
+    def path(self, value):
+        self.request.path = value
+
+    @property
+    def method(self):
+        return self.request.method
+
+    @method.setter
+    def method(self, value):
+        self.request.method = value.upper()
+
+    def __getitem__(self, name):
+        return self.request.headers[name]
 
     def __iter__(self):
-        pass
+        return iter(self.request.headers)
 
     def __len__(self):
-        pass
+        return len(self.request.headers)
+
+    def __contains__(self, item):
+        return item in self.request.headers
 
 
 class Response(response.Response):
     """Implements the response abstraction for cyclone.
     """
+
+    def __init__(self, handler):
+        self.handler = handler
+
+    def __setitem__(self, name, value):
+        self.handler.set_header(name, value)
+
+    def __getitem__(self, name):
+        # Cyclone doesn't provide a way to get headers normally, so break
+        # into the private methods to retrieve the header.  Note that
+        # this doesn't retrieve multi-value headers.  However, armet should
+        # handle multi-value wrangling itself.
+        return self.handler._headers[name]
