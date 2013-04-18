517b4b537b785614042a3fe1ee4a92d26617a358
Ryan Leckey <rleckey@concordusapps.com>
2013-04-18 11:25:19 -0700

    Add the package utilities back.

---
+++ src/armet/utils/package.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, division
+import os
+import pkgutil
+import importlib
+
+
+def import_module(name):
+    """Attempt to import a module; returns None if unsuccessful."""
+    try:
+        return importlib.import_module(name)
+
+    except ImportError:
+        return None
