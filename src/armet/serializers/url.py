729719d081de01983fe46c4d2ed7a2a58a20ad62
Erich Healy <ErichRHealy@gmail.com>
2013-04-26 15:32:03 -0700

    Add an application/x-www-form-urlencoded serializer for output.  Note that this serializer is rather limited in the kinds of data it can send down the pipe.

---
+++ src/armet/serializers/url.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals, division
+import six
+from .base import Serializer
+from armet import media_types
+
+# Import urlencode
+if six.PY3:
+    from urllib.parse import urlencode
+else:
+    from urllib import urlencode
+
+
+class URLSerializer(Serializer):
+
+    media_types = media_types.URL
+
+    def __init__(self, *args, **kwargs):
+        super(URLSerializer, self).__init__(*args, **kwargs)
+
+        # Passed to urlencode.  This causes array values to automatically be
+        # expanded properly instead of causing crazy nested percent encoding.
+        self.doseq = kwargs.get('doseq', True)
+
+        # # When encountering an array value, this will slap some php brackets
+        # # around it for easy php-ization.
+        # self.php_brackets = False
+
+    def serialize(self, obj=None):
+
+        #
+        if obj is None:
+            obj = {}
+
+        try:
+            super(URLSerializer, self).serialize(urlencode(obj, self.doseq))
+
+        except TypeError:
+            raise ValueError
