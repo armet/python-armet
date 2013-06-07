618025d61b28d1d8eb80c6e3df2e438f9e597c93
Ryan Leckey <rleckey@concordusapps.com>
2013-06-07 14:15:47 -0700

    Use session factory instead of engine for sqlalchemy.

--- src/armet/connectors/sqlalchemy/resources.py
+++ src/armet/connectors/sqlalchemy/resources.py
@@ -8,14 +8,11 @@ class ModelResourceOptions(object):
 
     def __init__(self, meta, name, bases):
         #! SQLAlchemy session used to perform operations on the models.
-        self.engine = meta.get('engine')
-        if not self.engine:
+        self.Session = meta.get('Session')
+        if not self.Session:
             raise ImproperlyConfigured(
-                'No engine specified for a model resource using SQLAlchemy.')
-
-        # Instantiate the sessionmaker and bind it to the engine.
-        from sqlalchemy.orm import sessionmaker
-        self.session = sessionmaker(bind=self.engine)
+                'A session factory (via sessionmaker) is required by '
+                'the SQLAlchemy model connector.')
 
 
 class ModelResource(object):
@@ -31,11 +28,10 @@ class ModelResource(object):
         super(ModelResource, self).__init__(*args, **kwargs)
 
         # Instantiate a session using our session object.
-        self.session = self.meta.session()
+        self.session = self.meta.Session()
 
     def read(self):
         # Initialize the queryset to the model manager.
-        # queryset = self.meta.model.objects
         queryset = self.session.query(self.meta.model)
 
         if self.slug is not None:
