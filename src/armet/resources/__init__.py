1dda9d543854d78727b9dc832d1cddeba83846c4
Ryan Leckey <rleckey@concordusapps.com>
2013-06-07 14:28:45 -0700

    Import all attributes into the resource namespace.

--- src/armet/resources/__init__.py
+++ src/armet/resources/__init__.py
@@ -3,10 +3,4 @@ from __future__ import absolute_import, unicode_literals, division
 from .resource import Resource
 from .managed import ManagedResource
 from .model import ModelResource
-
-
-__all__ = [
-    'Resource',
-    'ManagedResource',
-    'ModelResource'
-]
+from .attributes import *  # flake8: noqa
