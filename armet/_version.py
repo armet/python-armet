73ec2995ecc2ed67ed4c8807944f676c5b08e986
Ryan Leckey <rleckey@concordusapps.com>
2014-04-23 15:34:10 -0700

    Bump version.

--- armet/_version.py
+++ armet/_version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import, division
 
-__version_info__ = (0, 4, 33)
+__version_info__ = (0, 4, 34)
 __version__ = '.'.join(map(str, __version_info__))
