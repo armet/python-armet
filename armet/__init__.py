1e293828613da1782c4ee92d14e6fa22bca46c91
Erich Healy <ErichRHealy@gmail.com>
2014-07-01 18:48:57 -0700

    Don't expose all the things

--- armet/__init__.py
+++ armet/__init__.py
@@ -5,3 +5,10 @@ from . import encoders, decoders, codecs  # flake8: noqa
 # Register all modules in encoders and decoders
 codecs.register_module(encoders, encoders.Encoder)
 codecs.register_module(decoders, decoders.Decoder)
+
+
+__all__ = [
+    __version__,
+    encoders,
+    decoders
+]
