176784f3f0f9c9d050684de4fa8e4e3d4f3900cb
Erich Healy <ErichRHealy@gmail.com>
2014-07-09 16:15:42 -0700

    Refactor exceptions to utilize the werkzeug inheritance tree.  Exceptions are now full-fledged response objects.

--- armet/http/exceptions.py
+++ armet/http/exceptions.py
@@ -1,136 +1,124 @@
+from werkzeug import exceptions
 from http import client
+from collections import OrderedDict
+
+# Mirrors to werkzeug exceptions in the event we need to overload
+# these ourselves.
+Base = exceptions.HTTPException
 
 
-class Base(BaseException):
-    status = None
+class Base(exceptions.HTTPException):
 
-    def __init__(self, content=None, headers=None):
-        #! Body of the exception message.
-        self.content = content
+    def get_body(self, environ=None):
+        # There is no  body that needs to be returned for api exceptions.
+        return ''
 
-        #! Additional headers to place with the response.
-        self.headers = headers or {}
+    def get_headers(self, environ=None):
+        # Because we're no longer returning html, our content type is no
+        # longer text/html
+        # return [('Content-Type', 'text/plain')]
+        headers = OrderedDict(super().get_headers(environ))
+        headers['Content-Type'] = 'text/plain'
+        return headers.items()
 
 
-class BadRequest(Base):  # 400
-    status = client.BAD_REQUEST
+# The following exceptions are already implemented in werkzeug  Wrap them
+# in our exception class so they don't return html
+BadRequest = Base.wrap(exceptions.BadRequest)
+Unauthorized = Base.wrap(exceptions.Unauthorized)
+Forbidden = Base.wrap(exceptions.Forbidden)
+NotFound = Base.wrap(exceptions.NotFound)
+MethodNotAllowed = Base.wrap(exceptions.MethodNotAllowed)
+NotAcceptable = Base.wrap(exceptions.NotAcceptable)
+RequestTimeout = Base.wrap(exceptions.RequestTimeout)
+Conflict = Base.wrap(exceptions.Conflict)
+Gone = Base.wrap(exceptions.Gone)
+LengthRequired = Base.wrap(exceptions.LengthRequired)
+PreconditionFailed = Base.wrap(exceptions.PreconditionFailed)
+RequestEntityTooLarge = Base.wrap(exceptions.RequestEntityTooLarge)
+UnsupportedMediaType = Base.wrap(exceptions.UnsupportedMediaType)
+ExpectationFailed = Base.wrap(exceptions.ExpectationFailed)
+UnprocessableEntity = Base.wrap(exceptions.UnprocessableEntity)
+InternalServerError = Base.wrap(exceptions.InternalServerError)
+NotImplemented = Base.wrap(exceptions.NotImplemented)
+BadGateway = Base.wrap(exceptions.BadGateway)
+ServiceUnavailable = Base.wrap(exceptions.ServiceUnavailable)
+RequestedRangeNotSatisfiable = Base.wrap(
+    exceptions.RequestedRangeNotSatisfiable)
 
 
-class Unauthorized(Base):  # 401
-    status = client.UNAUTHORIZED
+# The following exceptions are not already implemented in werkzeug and require
+# that we create them ourselves.
 
 
 class PaymentRequired(Base):  # 402
-    status = client.PAYMENT_REQUIRED
-
-
-class Forbidden(Base):  # 403
-    status = client.FORBIDDEN
-
-
-class NotFound(Base):  # 404
-    status = client.NOT_FOUND
-
-
-class MethodNotAllowed(Base):  # 405
-    status = client.METHOD_NOT_ALLOWED
-
-    def __init__(self, allowed):
-        super(MethodNotAllowed, self).__init__(
-            headers={'Allow': ', '.join(allowed)})
-
-
-class NotAcceptable(Base):  # 406
-    status = client.NOT_ACCEPTABLE
+    code = client.PAYMENT_REQUIRED
+    description = (
+        'The server requires additional payment before '
+        'this resource can be accessed.'
+    )
 
 
 class ProxyAuthenticationRequired(Base):  # 407
-    status = client.PROXY_AUTHENTICATION_REQUIRED
-
-
-class RequestTimeout(Base):  # 408
-    status = client.REQUEST_TIMEOUT
-
-
-class Conflict(Base):  # 409
-    status = client.CONFLICT
-
-
-class Gone(Base):  # 410
-    status = client.GONE
-
-
-class LengthRequired(Base):  # 411
-    status = client.LENGTH_REQUIRED
-
-
-class PreconditionFailed(Base):  # 412
-    status = client.PRECONDITION_FAILED
-
-
-class RequestEntityTooLarge(Base):  # 413
-    status = client.REQUEST_ENTITY_TOO_LARGE
+    code = client.PROXY_AUTHENTICATION_REQUIRED
+    description = (
+        'The proxy requires authentication before '
+        'this resource can be accessed.'
+    )
 
 
 class RequestUriTooLong(Base):  # 414
-    status = client.REQUEST_URI_TOO_LONG
-
-
-class UnsupportedMediaType(Base):  # 415
-    status = client.UNSUPPORTED_MEDIA_TYPE
-
-
-class RequestedRangeNotSatisfiable(Base):  # 416
-    status = client.REQUESTED_RANGE_NOT_SATISFIABLE
-
-
-class ExpectationFailed(Base):  # 417
-    status = client.EXPECTATION_FAILED
-
-
-class UnprocessableEntity(Base):  # 422
-    status = client.UNPROCESSABLE_ENTITY
+    code = client.REQUEST_URI_TOO_LONG
+    description = ('The uri used to access this resource is too long.')
 
 
 class Locked(Base):  # 423
-    status = client.LOCKED
+    code = client.LOCKED
+    description = (
+        'This resource is currently locked.  '
+        'Please try again later.'
+    )
 
 
 class FailedDependency(Base):  # 424
-    status = client.FAILED_DEPENDENCY
+    code = client.FAILED_DEPENDENCY
+    description = (
+        'A prior request is reqired as a dependency for this request.'
+    )
 
 
 class UgradeRequired(Base):  # 426
-    status = client.UPGRADE_REQUIRED
-
-
-class InternalServerError(Base):  # 500
-    status = client.INTERNAL_SERVER_ERROR
-
-
-class NotImplemented(Base):  # 501
-    status = client.NOT_IMPLEMENTED
-
-
-class BadGateway(Base):  # 502
-    status = client.BAD_GATEWAY
-
-
-class ServiceUnavailable(Base):  # 503
-    status = client.SERVICE_UNAVAILABLE
+    code = client.UPGRADE_REQUIRED
+    description = (
+        'Client should re-attempt request using a different protocol.'
+    )
 
 
 class GatewayTimeout(Base):  # 504
-    status = client.GATEWAY_TIMEOUT
+    code = client.GATEWAY_TIMEOUT
+    description = (
+        'Gateway did not recieve a response from the upstream server.'
+    )
 
 
 class HttpVersionNotSupported(Base):  # 505
-    status = client.HTTP_VERSION_NOT_SUPPORTED
+    code = client.HTTP_VERSION_NOT_SUPPORTED
+    description = (
+        'Server does not support the http protocol version used to connect.'
+    )
 
 
 class InsufficientStorage(Base):  # 507
-    status = client.INSUFFICIENT_STORAGE
+    code = client.INSUFFICIENT_STORAGE
+    description = (
+        'Server has exceeded maximum storage capacity '
+        'required to complete this request'
+    )
 
 
-class NotExtended(Base):  # 510
-    status = client.NOT_EXTENDED
+class NotExtended(Base):
+    code = client.NOT_EXTENDED
+    description = (
+        'Further extensions to the request are required'
+        'for the server to fulfill it.'
+    )
