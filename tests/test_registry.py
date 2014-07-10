f042910128af514d1f656c1cbb9f94ebb3df85b2
Pholey <pholeyoh@gmail.com>
2014-07-10 16:42:08 -0700

    Stopping point for a second

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,17 +1,47 @@
 from armet.registry import Registry
+import pytest
 
 
 class TestRegistry:
 
     def setup(self):
         self.registry = Registry()
-        for x in range(10):
+        for x in range(5):
             self.registry.register("obj_%d" % x, name="test_%d" % x)
+            self.registry.register("obj_%d" % x, name2="test_%d" % x)
 
-    def test_register_with_same_name(self):
-        reg = self.registry
+    def test_register_and_find(self):
 
-        reg.register("")
+        # Test that fancy new register decorator
+        @self.registry.register(name="test_12")
+        def test_registry_decorator():
+            return "Example_obj"
 
+        self.registry.register("Example_obj2", name="test_13")
 
+        # test_registry_decorator()
 
+        assert self.registry.find(name="test_12") is test_registry_decorator
+
+    def test_raise_registry_exceptions(self):
+        # with pytest.raises(TypeError):
+        #     @self.registry.register(None, name="not_valid")
+
+        with pytest.raises(TypeError):
+            self.registry.find(name="this", multiple_kwargs="not_valid")
+
+        # Test the not-found exception in find, raises key-error, returns None.
+        assert self.registry.find(some_property="non_existant") is None
+
+    def test_remove(self):
+        # Test removing with just the object
+        # self.registry.remove("obj_1")
+        # assert self.registry.find(name="test_1") is None
+        # # assert that every single reference is removed
+        # assert self.registry.find(name2="test_1") is None
+
+        # Test removing with just kwargs:
+        self.registry.remove(name="test_2")
+        assert self.registry.find(name="test_2") is None
+        # assert that every single reference is removed
+        assert self.registry.find(name2="test_2") is None
