1f4647fcfaf2845938170e27e0d9b835d5e2f81e
Ryan Leckey <rleckey@concordusapps.com>
2014-07-11 10:45:17 -0700

    Split up tests for test_registry.

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -10,40 +10,39 @@ class TestRegistry:
             self.registry.register("obj_%d" % x, name="test_%d" % x)
             self.registry.register("obj_%d" % x, name2="test_%d" % x)
 
-    def test_register_and_find(self):
-
-        # Test that fancy new register decorator
+    def test_register_decorator(self):
         @self.registry.register(name="test_12")
         def test_registry_decorator():
-            return "Example_obj"
-
-        self.registry.register("Example_obj2", name="test_13")
-
-        # test_registry_decorator()
+            pass
 
         assert self.registry.find(name="test_12") is test_registry_decorator
 
-    def test_raise_registry_exceptions(self):
+    def test_register_none(self):
         with pytest.raises(TypeError):
             self.registry.register(name="not_valid")(None)
 
+    def test_find_multiple(self):
         with pytest.raises(TypeError):
             self.registry.find(name="this", multiple_kwargs="not_valid")
 
+    def test_find_not_found(self):
         # Test the not-found exception in find, raises key-error, returns None.
         assert self.registry.find(some_property="non_existant") is None
 
-    def test_remove(self):
+    def test_remove_object(self):
         # Test removing with just the object
         self.registry.remove("obj_1")
         assert self.registry.find(name="test_1") is None
+
         # assert that every single reference is removed
         assert self.registry.find(name2="test_1") is None
 
+    def test_remove_keyword(self):
         # Test removing with just kwargs:
         self.registry.remove(name="test_2")
 
         assert self.registry.find(name="test_2") is None
+
         # assert that every single reference is removed
         assert self.registry.find(name2="test_2") is None
 
@@ -51,8 +50,7 @@ class TestRegistry:
 
         assert self.registry.find(name2="test_3") is None
 
-        self.registry.remove()
-
+    def test_remove_empty_key(self):
         # Test removing the entire "test" key if it holds no items
         self.registry.register("obj_1", test="test_1")
         self.registry.remove(test="test_1")
